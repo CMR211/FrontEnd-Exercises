41f88b7bae15ca1cb9b64f2c4de76675231e648c
cmr211 <bartosz.surma211@gmail.com>
2021-11-03 15:51:46 +0100

    class components -> function components

--- 05_ProjectControl/browser-react/App.js
+++ 05_ProjectControl/browser-react/App.js
@@ -1,17 +1,88 @@
-class App extends React.Component {
-    render () {
+const project = new Object();
+
+
+function ChangeableText (props) {
+
+    const divID = props.id;
+
+    const [renameState, setRenameState] = React.useState();
+    const [inputText, setInputText] = React.useState();
+
+    const handleInput = (event) => {
+        project[divID] = event.target.value;
+        setInputText(project[divID]);
+        console.log(project);
+    }
+
+    const inputSize = {
+        display: "inline", 
+        fontSize: "1.5em",
+        fontFamily: "inherit",
+        flexGrow: "1",
+        padding: "0",
+        margin: "auto 0.5em",
+        border: "none"
+    }
+
+    const divStyle = {
+        display: "flex",
+        width: "20em",
+        backgroundColor: "yellow",
+        padding: "0",
+        margin: "0",
+    }
+
+    if (renameState) {
         return (
-            <React.Fragment>
-                <div className="project-info">
-                    {/* <h1>Nazwa zadania</h1> */}
-                    {/* <ButtonSmall type="add"/><br /> */}
-                    <ChangeableText projectName="pracownia"/>
-                    {/* <Input placeholder="Write sth."/> */}
-                </div>
-            </React.Fragment>
+            <div id={divID} style={divStyle}>
+                <input 
+                    value= {project[divID] || "Enter project name..."} 
+                    onChange={handleInput}
+                    style={inputSize}/>
+                <ButtonSmall 
+                    type="confirm" 
+                    onClick={() => setRenameState(false)}
+                />
+            </div>
+        )
+    } else {
+        return (
+            <div id={divID} style={divStyle}>
+                <p style={inputSize}> 
+                    {inputText} 
+                </p>
+                <ButtonSmall 
+                    type="rename" 
+                    onClick={() => setRenameState(true)} 
+                />
+            </div>
         )
     }
 }
 
+function ButtonSmall (props) {
+    return (
+        <button 
+        onClick={props.onClick}
+        className="button-small">
+            <img 
+            src={"./Icons/icon_" + props.type + ".svg"} />
+        </button>
+    )
+}
+
+function App () {
+    return (
+        <React.Fragment>
+            <div className="project-info">
+                <ChangeableText id="nazwaZadania" />
+                <ChangeableText id="inwestor" />
+            </div>
+        </React.Fragment>
+    )
+}
+
 const root = document.getElementById("react-app");
+
 ReactDOM.render(<App />, root);
+
--- 05_ProjectControl/browser-react/Components/Input/Input.js
+++
@@ -1,7 +0,0 @@
-class Input extends React.Component {
-    render () {
-        return (
-            <input className="input" type="text" placeholder={this.props.placeholder}/>
-        )
-    }
-}
