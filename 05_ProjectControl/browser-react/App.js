65513d8516fae6a9472cd0796ba584f4dfa2baec
cmr211 <bartosz.surma211@gmail.com>
2021-11-08 15:48:30 +0100

    added tabs'

--- 05_ProjectControl/browser-react/App.js
+++ 05_ProjectControl/browser-react/App.js
@@ -1,250 +1,274 @@
 const project = new Object();
 project.mpzp = "KDW 2.01";
+project.nazwa =
+  "Przebudowa drogi wojeódzkiej nr 441 polegająca na budowie chodnika wzdłuż ulicy Kaliskiej w miejsocowści Syców";
 
-function ChangeableText (props) {
+const layout = new Object();
+layout.activePanel = "Projektowanie";
 
-    const divID = props.id;
+function ChangeableText(props) {
+  const divID = props.id;
 
-    const [renameState, setRenameState] = React.useState();
-    const [inputText, setInputText] = React.useState();
+  const [renameState, setRenameState] = React.useState();
+  const [inputText, setInputText] = React.useState();
 
-    const handleInput = (event) => {
-        project[divID] = event.target.value;
-        setInputText(project[divID]);
-        console.log(project);
-    }
+  const handleInput = (event) => {
+    project[divID] = event.target.value;
+    setInputText(project[divID]);
+    console.log(project);
+  };
 
-    const inputSize = {
-        display: "block", 
-        fontSize: "1em",
-        fontFamily: "inherit",
-        flexGrow: "1",
-        padding: "0",
-        margin: "auto 0.5em",
-        border: "none"
-    }
+  const inputSize = {
+    display: "block",
+    fontSize: "1em",
+    flexGrow: "1",
+    padding: "0",
+    margin: "auto 0.5em",
+    border: "none",
+  };
 
-    const divStyle = {
-        display: "flex",
-        width: "20em",
-        backgroundColor: "yellow",
-        padding: "0",
-        margin: "0",
-    }
+  const divStyle = {
+    display: "flex",
+    width: "20em",
+    padding: "0",
+    margin: "0",
+  };
 
-    if (renameState) {
-        return (
-            <div id={divID} style={divStyle}>
-                <input 
-                    value= {project[divID] || "Enter project name..."} 
-                    onChange={handleInput}
-                    style={inputSize}/>
-                <ButtonSmall 
-                    type="confirm" 
-                    onClick={() => setRenameState(false)}
-                />
-            </div>
-        )
-    } else {
-        return (
-            <div id={divID} style={divStyle}>
-                <p style={inputSize}> 
-                    {inputText} 
-                </p>
-                <ButtonSmall 
-                    type="rename" 
-                    onClick={() => setRenameState(true)} 
-                />
-            </div>
-        )
-    }
+  if (renameState) {
+    return (
+      <div id={divID} style={divStyle}>
+        <input
+          value={project[divID] || "Enter project name..."}
+          onChange={handleInput}
+          style={inputSize}
+        />
+        <ButtonSmall type="confirm" onClick={() => setRenameState(false)} />
+      </div>
+    );
+  } else {
+    return (
+      <div id={divID} style={divStyle}>
+        <p style={inputSize}>{inputText}</p>
+        <ButtonSmall type="rename" onClick={() => setRenameState(true)} />
+      </div>
+    );
+  }
 }
 
-function ButtonSmall (props) {
-    
-    const [isHover, setHover] = React.useState();
+function ButtonSmall(props) {
+  const [isHover, setHover] = React.useState();
 
-    const handleMouseEnter = () => {
-        setHover(true);
-    }
-    const handleMouseLeave = () => {
-        setHover(false);
-    }
+  const handleMouseEnter = () => {
+    setHover(true);
+  };
 
-    let hoverText;
-    isHover ? hoverText = `${props.type.toUpperCase()}` : "";
+  const handleMouseLeave = () => {
+    setHover(false);
+  };
 
-    return (
-        <div>
-            <button 
-            onClick={props.onClick}
-            className="button-small"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}>
-                <div style={{display:"flex", alignItems: "center", justifyContent:"center"}}>
-                    <img src={"./Icons/icon_" + props.type + ".svg"} />
-                    <span>{hoverText}</span>
-                </div>
-            </button>
+  let cursorStyle = "default";
+
+  const buttonStyle = {
+    cursor: cursorStyle,
+  };
+
+  let hoverText;
+  isHover ? (hoverText = `${props.type.toUpperCase()}`) : "";
+  isHover ? (cursorStyle = "pointer") : (cursorStyle = "default");
+
+  return (
+    <div>
+      <button
+        onClick={props.onClick}
+        className="button-small"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        style={buttonStyle}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+          <img src={"./Icons/icon_" + props.type + ".svg"} />
+          <span>{hoverText}</span>
         </div>
-    )
+      </button>
+    </div>
+  );
 }
 
-function LabeledContainer (props) {
-    return(
-        <div id={props.id} className={props.className}>
-            <h1>{props.title}</h1>
-            {props.children}
-        </div>
-    )
+function LabeledContainer(props) {
+  return (
+    <div id={props.id} className={props.className}>
+      <h1>{props.title}</h1>
+      {props.children}
+    </div>
+  );
 }
 
-function EditableText (props) {
+function EditableText(props) {
+  const [value, setValue] = React.useState(project[props.data]);
+  const [opacity, setOpacity] = React.useState("20%");
+  const [mouseStyle, setMouseStyle] = React.useState("default");
 
-    const [value, setValue] = React.useState(project[props.data]);
-    const [opacity, setOpacity] = React.useState("20%");
-    const [mouseStyle, setMouseStyle] = React.useState("default");
+  const divStyle = {
+    display: "flex",
+    width: "100%",
+  };
 
-    const divStyle = {
-        display: "flex",
-        width: "100%",
-    }
+  const buttonStyle = {
+    backgroundColor: "transparent",
+    opacity: opacity,
+    margin: "0 0 0 0.5em",
+    cursor: mouseStyle,
+  };
 
-    const buttonStyle = {
-        opacity: opacity,
-        margin: "0 0 0 0.5em",
-        cursor: mouseStyle,
-    }
+  const handleEdit = () => {
+    project[props.data] = prompt(props.prompt) || project[props.data];
+    setValue(project[props.data]);
+  };
 
-    const handleEdit = () => {
-        project[props.data] = prompt(props.prompt) || project[props.data];
-        setValue(project[props.data]);
-    }
+  const handleMouseEnter = () => {
+    setOpacity("100%");
+    setMouseStyle("pointer");
+  };
 
-    const handleMouseEnter = () => {
-        setOpacity("100%");
-        setMouseStyle("pointer");
-    }
+  const handleMouseLeave = () => {
+    setOpacity("20%");
+    setMouseStyle("default");
+  };
 
-    const handleMouseLeave = () => {
-        setOpacity("20%");
-        setMouseStyle("default");
-    }
+  return (
+    <div style={divStyle}>
+      <p>
+        {props.label}: {value}
+      </p>
+      <button
+        style={buttonStyle}
+        onClick={handleEdit}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
+        (edytuj)
+      </button>
+    </div>
+  );
+}
 
+function EditableLink(props) {
+  const [mouseStyle, setMouseStyle] = React.useState("default");
+
+  const handleMouseEnter = () => {
+    setMouseStyle("pointer");
+  };
+
+  const handleMouseLeave = () => {
+    setMouseStyle("default");
+  };
+
+  const style = {
+    cursor: mouseStyle,
+    width: "3em",
+  };
+
+  return (
+    <a href="https://www.youtube.com" target="_blank">
+      <img
+        src="./Icons/youtube.svg"
+        alt="youtube"
+        style={style}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      />
+    </a>
+  );
+}
+
+function NavBar(props) {
+
+  return (
+    <LabeledContainer
+      id="container-navbar"
+      className="paper"
+      title={<ChangeableText id="nazwa"/>}> 
+      <div style={{display: "flex", flexFlow: "row", justifyContent: "center"}}>
+        <NavItem name="Projektowanie" activePanel={props.activePanel} changePanel={props.changePanel}/>
+        <NavItem name="Uzgodnienia" activePanel={props.activePanel}  changePanel={props.changePanel}/>
+        <NavItem name="Zgłoszenie" activePanel={props.activePanel}  changePanel={props.changePanel}/>  
+      </div>        
+    </LabeledContainer>
+  )
+}
+
+function NavItem(props) {
+
+  let border;
+  if (props.activePanel === props.name) {
+    border = "2px solid black"
+  };
+  
+  const style = {
+    padding: "1em",
+    margin: "1em",
+    borderRadius: "6px",
+    fontSize: "1em",
+    border: border,
+  }
+
+  return (
+    <button style={style} className="btn-navitem" id={`btn-navitem-${props.name}`} onClick={() => props.changePanel(props.name)}>
+      {props.name}
+    </button>
+  )
+}
+
+function ProjectContainer (props) {
+
+  const [activePanel, setActivePanel] = React.useState("Uzgodnienia");
+
+  function changePanel(i) {
+    setActivePanel(i);
+  }
+
+  if (activePanel == "Projektowanie") {
     return (
-        <div style={divStyle}>
-            <p>{props.label}: {value}</p>
-            <button 
-                style={buttonStyle} 
-                onClick={handleEdit} 
-                onMouseEnter={handleMouseEnter} 
-                onMouseLeave={handleMouseLeave}>
-                (edytuj)
-            </button>
-        </div>
+      <div>
+        <NavBar activePanel={activePanel} changePanel={changePanel}/>
+        <LabeledContainer title="Wymagania zamawiającego" className="paper">
+          <p>asd</p>
+        </LabeledContainer>
+      </div>
     )
+  }
+
+  if (activePanel == "Uzgodnienia") {
+    return (
+      <div>
+        <NavBar activePanel={activePanel} changePanel={changePanel}/>
+        <p>zxc</p>
+      </div>
+    )
+  }
+
+  if (activePanel == "Zgłoszenie") {
+    return (
+      <div>
+        <NavBar activePanel={activePanel} changePanel={changePanel}/>
+        <p>qwe</p>
+      </div>
+    )
+  }
+
 }
 
-function EditableLink (props) {
-
-    const [mouseStyle, setMouseStyle] = React.useState("default");
-
-    const handleMouseEnter = () => {
-        setMouseStyle("pointer");
-    }
-
-    const handleMouseLeave = () => {
-        setMouseStyle("default");
-    }
-
-    const style = {
-        cursor: mouseStyle,
-        width: "3em"
-    }
-
-    return (
-        <a href="https://www.youtube.com" target="_blank">
-            <img 
-                src="./Icons/youtube.svg" 
-                alt="youtube" 
-                style={style}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-            />
-        </a>
-    )
-}
-
-
-function App () {
-    return (
-            <div className="main-container">
-                <LabeledContainer id="container-top" className="paper" title="Informacje podstawowe">
-                    <div className="third-container" id="header-sub-container">
-                        <div className="fourth-container">
-                            <h2>Inwestor/zamawiający</h2>
-                            <ChangeableText id="inwestor" />    
-                        </div>
-                        <div className="fourth-container">
-                            <h2>Nazwa zadania</h2>
-                            <ChangeableText id="nazwaZadania" />
-                        </div>
-                        <div className="fourth-container">
-                            <h2>Działki</h2>
-                        </div>
-                    </div>
-                </LabeledContainer>
-
-                <LabeledContainer id="container-left-up" className="paper" title="Informacje do projektowania">
-                    <EditableText 
-                        label="Link do YT" 
-                        data="youtube"
-                        prompt="Podaj link do filmu na youtube.com:"
-                    />
-                    <EditableText 
-                        label="MPZP" 
-                        data="mpzp"
-                        prompt="Podaj zagospodarowanie przestrzenne dla tej drogi:"
-                    />
-                    <EditableText 
-                        label="Publiczna" 
-                        data="czyPubliczna"
-                        prompt="Czy droga jest drogą publiczną? (tak/nie) Jeśli tak możesz podać jej nadany numer."
-                    />
-                    <EditableText 
-                        label="Szerokość jezdni" 
-                        data="szerokoscJezdni"
-                        prompt="Podaj wymaganą przez zamawiającego szerokość jezdni."
-                    />
-                
-                </LabeledContainer>
-
-                <LabeledContainer id="container-left-middle" className="paper" title="Linki">
-                    <EditableLink />
-                </LabeledContainer>
-
-                <LabeledContainer id="container-left-down" className="paper" title="Progres w rysunkach">
-                    <input type="checkbox" name="mpzp"/>
-                    <label for="mpzp">Mapa poglądowa</label>
-                </LabeledContainer>
-
-                <LabeledContainer id="container-middle" className="paper" title="Uzgodnienia">
-
-                </LabeledContainer>
-
-                <LabeledContainer id="container-right" className="paper" title="Zgłoszenie">
-
-                </LabeledContainer>
-
-                <LabeledContainer id="container-bottom" className="paper" title="Nawigacja">
-
-                </LabeledContainer>
-
-            </div>
-    )
+function App() {
+  return (
+    <React.Fragment>
+      <ProjectContainer />    
+    </React.Fragment>
+  );
 }
 
 const root = document.getElementById("react-app");
 
 ReactDOM.render(<App />, root);
-
