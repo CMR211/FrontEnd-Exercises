4c9976795a14d51b57e236b828b6bbac4e25603e
cmr211 <bartosz.surma211@gmail.com>
2021-11-23 15:59:53 +0100

    a

---
+++ 08-front-end-mentors-space-tourism-website/src/react.js
@@ -0,0 +1,17 @@
+function App () {
+    return (
+        <div>
+            <h1>Heading 1</h1>
+            <h2>Heading 2</h2>
+            <h3>Heading 3</h3>
+            <h4>Heading 4</h4>
+            <h5>Heading 5</h5>
+            <p class="subheading1">Subheading 1</p>
+            <p class="subheading2">Subheading 1</p>
+            <p class="navtext">Nav text</p>
+            <p class="bodytext">Body text</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("react-app"));
