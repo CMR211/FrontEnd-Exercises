956eda18e1594322762dbdaf99b2763901190bcf
cmr211 <bartosz.surma211@gmail.com>
2021-11-25 14:50:05 +0100

    destinations page first draft

--- 08-front-end-mentors-space-tourism-website/src/react.js
+++ 08-front-end-mentors-space-tourism-website/src/react.js
@@ -1,3 +1,13 @@
+ReactDOM.render(<App />, document.getElementById("react-app"));
+
+
+
+
+
+
+
+
+
 // New hook to get current viewport dimensions
 
 function getWindowWidth() {
@@ -17,24 +27,49 @@ function useWindowDimensions() {
   return windowWidth;
 }
 
+
+
+
+
+
+
+
+
 function App() {
 
   // Global language state: 0 is english, 1 is polish
   const [lang, setLang] = React.useState(0);
+  
+  // Current subpage state
+  const [currentPage, setCurrentPage] = React.useState(1);
 
   // Global state of window size to determine various comonents visibility
   const windowWidth = useWindowDimensions();
 
   return (
     <React.Fragment>
-      <Navbar language={lang} setLang={setLang} windowWidth={windowWidth}/>
-      <FrontPage language={lang} />
-      {/* <Destination /> */}
+      <Navbar 
+        language={lang} 
+        setLang={setLang} 
+        windowWidth={windowWidth} 
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}/>
+      {currentPage === 0 ? <FrontPage language={lang} /> : null}
+      {currentPage === 1 ? <DestinationPage language={lang} /> : null}
     </React.Fragment>
   )
 }
 
-function Navbar ( {setLang, language} ) {
+
+
+
+
+
+
+
+
+
+function Navbar ( {setLang, language, setCurrentPage, currentPage} ) {
 
   const [isActive, setActive] = React.useState(false);
 
@@ -42,29 +77,66 @@ function Navbar ( {setLang, language} ) {
     return (window.innerWidth >= 768)
   }
 
-  function toggleActive () { setActive(!isActive) };
+  function toggleActive () { setActive(!isActive) }
+
+  function goToPage(pagenumber) { setCurrentPage(pagenumber) }
   
   function MobileNavbar () {
     return (
-      <div 
-      id="mobile-navbar"
-      style={{display: isActive ? "flex" : "none" }}>
-        <div>
-          <p className="navtext">00</p>
+        <div 
+        id="mobile-navbar"
+        style={{display: isActive ? "flex" : "none" }}>
+          <div id="touch-space" onClick={toggleActive}>
+            {/* empty space */}
+          </div>
+          <div onClick={() => goToPage(0)}>
+            <p className="navtext">00</p>
+            <p style={{color: (currentPage === 0) ? "gray" : "white"}}
+              className="navtext">{DATA.headings.navbar.home[language]}</p>
+          </div>
+          <div onClick={() => goToPage(1)}>
+            <p className="navtext">01</p>
+            <p style={{color: (currentPage === 1) ? "gray" : "white"}}
+              className="navtext">{DATA.headings.navbar.destination[language]}</p>
+          </div>
+          <div onClick={() => goToPage(2)}>
+            <p className="navtext">02</p>
+            <p style={{color: (currentPage === 2) ? "gray" : "white"}}
+              className="navtext">{DATA.headings.navbar.crew[language]}</p>
+          </div>
+          <div onClick={() => goToPage(3)}>
+            <p className="navtext">03</p>
+            <p style={{color: (currentPage === 3) ? "gray" : "white"}}
+              className="navtext">{DATA.headings.navbar.technology[language]}</p>
+          </div>
+        </div>
+    )
+  }
+
+  function FullscreenNavbar () {
+    return (
+      <div id="fullscreen-navbar"
+           style={{display: isBigScreen() ? "flex" : "none"}}>
+        <div 
+          onClick={() => setCurrentPage(0)}
+          style={{borderBottom: (currentPage === 0) ? "solid 4px white" : "solid 4px transparent"}}>
           <p className="navtext">{DATA.headings.navbar.home[language]}</p>
-        </div>
-        <div>
-          <p className="navtext">01</p>
+        </div> 
+        <div 
+          onClick={() => setCurrentPage(1)}
+          style={{borderBottom: (currentPage === 1) ? "solid 4px white" : "solid 4px transparent"}}>
           <p className="navtext">{DATA.headings.navbar.destination[language]}</p>
-        </div>
-        <div>
-          <p className="navtext">02</p>
+        </div>  
+        <div 
+          onClick={() => setCurrentPage(2)}
+          style={{borderBottom: (currentPage === 2) ? "solid 4px white" : "solid 4px transparent"}}>
           <p className="navtext">{DATA.headings.navbar.crew[language]}</p>
-        </div>
-        <div>
-          <p className="navtext">03</p>
+        </div>  
+        <div 
+          onClick={() => setCurrentPage(3)}
+          style={{borderBottom: (currentPage === 3) ? "solid 4px white" : "solid 4px transparent"}}>
           <p className="navtext">{DATA.headings.navbar.technology[language]}</p>
-        </div>
+        </div>   
       </div>
     )
   }
@@ -79,26 +151,37 @@ function Navbar ( {setLang, language} ) {
           <button className="btn-lang" onClick={() => setLang(0)}>EN</button>
         </div>    
       </div>
-      <button 
-        className="btn-hamburger btn-lang" 
-        onClick={toggleActive} 
-        style={{
-         display: isBigScreen() ? "none" : "block",
-         display: isActive ? "none" : "block"}}>
-        <img src="./assets/shared/icon-hamburger.svg" />
-      </button>
-      <button 
-        className="btn-hamburger btn-lang" 
-        onClick={toggleActive} 
-        style={{
-          display: isBigScreen() ? "none" : "block",
-          display: isActive ? "block" : "none",
-          zIndex: 2}}>
-            <img src="./assets/shared/icon-close.svg" />
+      <FullscreenNavbar language={language} />
+      <div 
+      id="navbar-btn-container" 
+      style={{display: isBigScreen() ? "none" : "block"}}>
+        <button 
+          className="btn-hamburger btn-lang" 
+          onClick={toggleActive} 
+          style={{
+          display: (!isBigScreen() && !isActive) ? "block" : "none"}}>
+          <img src="./assets/shared/icon-hamburger.svg" />
         </button>
+        <button 
+          className="btn-hamburger btn-lang" 
+          onClick={toggleActive} 
+          style={{
+            display: (!isBigScreen() && isActive) ? "block" : "none",
+            zIndex: 2}}>
+              <img src="./assets/shared/icon-close.svg" />
+          </button>
+        </div>
     </div>
   )
 }
+
+
+
+
+
+
+
+
 
 function FrontPage ( {language} ) {
 
@@ -114,13 +197,45 @@ function FrontPage ( {language} ) {
   );
 }
 
-function Destination () {
-  return (
-    <div>
 
+
+
+
+
+
+
+
+function DestinationPage ( {language} ) {
+
+  const [currentPlanet, setCurrentPlanet] = React.useState(0);
+
+  return (
+    <div id="destinationpage">
+      <h5>
+        <span>01</span>
+        {DATA.headings.destinationpage.title[language]}
+      </h5>
+      <div>
+        <img src=`./assets/destination/image-${DATA.destinations[currentPlanet].name}.webp` />
+        <div id="planet-navbar">
+          <p className="navtext">{DATA.destinations[0].name[language]}</p>
+          <p className="navtext">{DATA.destinations[1].name[language]}</p>
+          <p className="navtext">{DATA.destinations[2].name[language]}</p>
+          <p className="navtext">{DATA.destinations[3].name[language]}</p>
+        </div>
+        <h2>{DATA.destinations[currentPlanet].name[language]}</h2>
+      </div>
     </div>
   )
 }
+
+
+
+
+
+
+
+
 
 function DesignPage() {
   return (
@@ -138,4 +253,4 @@ function DesignPage() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("react-app"));
+
