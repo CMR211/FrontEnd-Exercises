9a2945d561444c4044d2818026bd2a06251f0edc
cmr211 <bartosz.surma211@gmail.com>
2021-12-06 15:59:44 +0100

    euler3

--- 10-project-euler/problem-3.js
+++ 10-project-euler/problem-3.js
@@ -1,3 +1,8 @@
+// 3. Largest prime factor
+
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
 function generatePrimes (upperLimit) {
   const primes = [2];
   for (let i=3; i < upperLimit; i++) {
